25b84b1cbb5e9a3f4a20523cba3f2d59e3403b19
Stefan Sundin <git@stefansundin.com>
2016-09-07 14:37:38 -0700

    Create the new key and replace the old one in the credentials file.

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"os"
 	"os/user"
+	"regexp"
+	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
@@ -24,43 +29,106 @@ func main() {
 	credentialsPath := fmt.Sprintf("%s/.aws/credentials", usr.HomeDir)
 	credentialsProvider := credentials.NewSharedCredentials(credentialsPath, profileFlag)
 	creds, err := credentialsProvider.Get()
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
+	check(err)
 	fmt.Printf("Using access key %s from profile \"%s\".\n", creds.AccessKeyID, profileFlag)
 
 	// Create session
 	sess, err := session.NewSession(&aws.Config{Credentials: credentialsProvider})
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
+	check(err)
 
 	// sts get-caller-identity
 	stsClient := sts.New(sess)
 	respGetCallerIdentity, err := stsClient.GetCallerIdentity(&sts.GetCallerIdentityInput{})
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-	fmt.Printf("Your user arn is: %s\n", *respGetCallerIdentity.Arn)
+	check(err)
+	fmt.Printf("Your user arn is: %s\n\n", *respGetCallerIdentity.Arn)
 
 	// iam list-access-keys
 	// If the UserName field is not specified, the UserName is determined implicitly based on the AWS access key ID used to sign the request.
 	iamClient := iam.New(sess)
 	respListAccessKeys, err := iamClient.ListAccessKeys(&iam.ListAccessKeysInput{})
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
+	check(err)
 
 	// Print key information
 	fmt.Printf("You have %d access key%v associated with your user:\n", len(respListAccessKeys.AccessKeyMetadata), pluralize(len(respListAccessKeys.AccessKeyMetadata)))
 	for _, key := range respListAccessKeys.AccessKeyMetadata {
-		fmt.Printf("- %s (created %s)\n", *key.AccessKeyId, key.CreateDate)
+		fmt.Printf("- %s (%s, created %s)\n", *key.AccessKeyId, *key.Status, key.CreateDate)
 	}
 	fmt.Println()
+
+	if len(respListAccessKeys.AccessKeyMetadata) == 2 {
+		var deleteAccessKeyID *string
+		if *respListAccessKeys.AccessKeyMetadata[0].AccessKeyId == creds.AccessKeyID {
+			deleteAccessKeyID = respListAccessKeys.AccessKeyMetadata[1].AccessKeyId
+		} else {
+			deleteAccessKeyID = respListAccessKeys.AccessKeyMetadata[0].AccessKeyId
+		}
+
+		if yesFlag == false {
+			fmt.Println("You have two access keys, which is the max number of access keys.")
+			fmt.Printf("Do you want to delete %s and create a new key? [yN] ", *deleteAccessKeyID)
+			reader := bufio.NewReader(os.Stdin)
+			yn, err2 := reader.ReadString('\n')
+			check(err2)
+			if yn[0] != 'y' && yn[0] != 'Y' {
+				return
+			}
+		}
+
+		_, err2 := iamClient.DeleteAccessKey(&iam.DeleteAccessKeyInput{
+			AccessKeyId: deleteAccessKeyID,
+		})
+		check(err2)
+		fmt.Printf("Deleted access key %s.\n", *deleteAccessKeyID)
+	} else if yesFlag == false {
+		fmt.Printf("Do you want to create a new key and deactivate %s? [yN] ", *respListAccessKeys.AccessKeyMetadata[0].AccessKeyId)
+		reader := bufio.NewReader(os.Stdin)
+		yn, err2 := reader.ReadString('\n')
+		check(err2)
+		if yn[0] != 'y' && yn[0] != 'Y' {
+			return
+		}
+	}
+
+	// Create the new access key
+	// If you do not specify a user name, IAM determines the user name implicitly based on the AWS access key ID signing the request.
+	respCreateAccessKey, err := iamClient.CreateAccessKey(&iam.CreateAccessKeyInput{})
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		return
+	}
+	fmt.Printf("Created access key %s.\n", *respCreateAccessKey.AccessKey.AccessKeyId)
+
+	// Read credentials file
+	bytes, err := ioutil.ReadFile(credentialsPath)
+	check(err)
+	text := string(bytes)
+
+	// Replace key pair in credentials file
+	// This search & replace does not limit itself to the specified profile, which may be useful if the user is using the same key in multiple profiles
+	re := regexp.MustCompile(fmt.Sprintf(`(?m)^aws_access_key_id ?= ?%s`, regexp.QuoteMeta(creds.AccessKeyID)))
+	text = re.ReplaceAllString(text, `aws_access_key_id=`+*respCreateAccessKey.AccessKey.AccessKeyId)
+	re = regexp.MustCompile(fmt.Sprintf(`(?m)^aws_secret_access_key ?= ?%s`, regexp.QuoteMeta(creds.SecretAccessKey)))
+	text = re.ReplaceAllString(text, `aws_secret_access_key=`+*respCreateAccessKey.AccessKey.SecretAccessKey)
+
+	// Verify that the regexp actually replaced something
+	if !strings.Contains(text, *respCreateAccessKey.AccessKey.AccessKeyId) || !strings.Contains(text, *respCreateAccessKey.AccessKey.SecretAccessKey) {
+		fmt.Println("Failed to replace old access key. Aborting.")
+		fmt.Printf("Please verify that the file %s is formatted correctly.\n", credentialsPath)
+		os.Exit(1)
+	}
+
+	// Write new file
+	err = ioutil.WriteFile(credentialsPath, []byte(text), 0600)
+	check(err)
+	fmt.Printf("Wrote new key pair to %s\n", credentialsPath)
+
+	_, err = iamClient.UpdateAccessKey(&iam.UpdateAccessKeyInput{
+		AccessKeyId: &creds.AccessKeyID,
+		Status:      aws.String("Inactive"),
+	})
+	check(err)
+	fmt.Printf("Deactivated old access key %s.\n", creds.AccessKeyID)
+	fmt.Println("Please make sure this key is not used elsewhere.")
 }
 
 func pluralize(n int) string {
@@ -68,4 +136,11 @@ func pluralize(n int) string {
 		return ""
 	}
 	return "s"
+}
+
+func check(err error) {
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
 }
